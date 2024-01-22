62c2bcba66f99994762ee95c5fefdc918ca32008
YuriiHolyda <mescyura@gmail.com>
2024-01-22 10:38:15 +0200

    gitignore and add closing mobile menu when click on link

--- js/menu-open.js
+++ js/menu-open.js
@@ -1,14 +1,19 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuBtnClose = document.querySelectorAll('.menu-btn-close');
 
-const toggleMenu = () =>
-    mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 const disableScroll = () =>
-    document.body.classList.toggle('is-scroll-disabled');
+  document.body.classList.toggle('is-scroll-disabled');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+
+menuBtnClose.forEach(function (e) {
+  e.addEventListener('click', toggleMenu);
+});
 
 menuBtnOpen.addEventListener('click', disableScroll);
-menuBtnClose.addEventListener('click', disableScroll);
+
+menuBtnClose.forEach(function (e) {
+  e.addEventListener('click', disableScroll);
+});
